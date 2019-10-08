4ee400c2a973db7ca8ffc24de5ade14a9ec79fda
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-08 23:34:58 +0300

    Scripted auto-commit on change (2019-10-08 23:34:58) by gitwatch.sh

--- src/main/java/MyComporator.java
+++ src/main/java/MyComporator.java
@@ -2,6 +2,8 @@ import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.WritableComparable;
 
 public class MyComporator implements RawComparator {
-    public int compare(WritableComparable a,WritableComparable b);
-    return a.c
+    @Override
+    public int compare(WritableComparable a,WritableComparable b) {
+        return 1;
+    }
 }
