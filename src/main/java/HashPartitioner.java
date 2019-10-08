426be8f84449602b67c2806289a224cf4fc301bc
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-08 23:17:07 +0300

    Scripted auto-commit on change (2019-10-08 23:17:07) by gitwatch.sh

--- src/main/java/HashPartitioner.java
+++ src/main/java/HashPartitioner.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
+import org.apache.hadoop.mapreduce.Partitioner;
 
-public class HashPartitioner {
+public class HashPartitioner<K, V> extends Partitioner<K, V> { public int getPartition(K key, V value, int numReduceTasks) {
+    return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks; }
 }
