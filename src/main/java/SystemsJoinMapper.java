41cdcd58d21c823243e4dab32b6ac9d424fc2f17
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-08 16:25:33 +0300

    Scripted auto-commit on change (2019-10-08 16:25:33) by gitwatch.sh

--- src/main/java/SystemsJoinMapper.java
+++ src/main/java/SystemsJoinMapper.java
@@ -1,2 +1,16 @@
-public class SystemsJoinMapper {
+
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.Text;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class SystemsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        SystemInfo system = new SystemInfo(value);
+        context.write(new TextPair(system.getSystemCode().toString(),"0"), new Text(system.toString())); }
 }
