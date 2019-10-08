85be9afa1f80eadf441c23c20034863a9a0375a0
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-08 16:27:21 +0300

    Scripted auto-commit on change (2019-10-08 16:27:21) by gitwatch.sh

--- src/main/java/WordCountApp.java
+++ src/main/java/WordCountApp.java
@@ -6,24 +6,30 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
 public class WordCountApp {
-  public static void main(String[] args) throws Exception {
-    if (args.length != 2) {
-      System.err.println("Usage: WordCountApp <input path> <output path>");
-      System.exit(-1);
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(JoinJob.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
-      Job job = Job.getInstance();
-      job.setJarByClass(WordCountApp.class);
-      job.setJobName("Word count");
-      FileInputFormat.addInputPath(job, new Path(args[0]));
-      FileOutputFormat.setOutputPath(job, new Path(args[1]));
-      job.setMapperClass(WordMapper.class);
-      job.setReducerClass(WordReducer.class);
-      job.setOutputKeyClass(Text.class);
-      job.setOutputValueClass(IntWritable.class);
-      System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
-  }
+}
