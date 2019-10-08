8dbb08320cf78d75efc233e929ac2472d60bb895
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-08 23:31:18 +0300

    Scripted auto-commit on change (2019-10-08 23:31:18) by gitwatch.sh

--- src/main/java/ReduceJoinApp.java
+++ src/main/java/ReduceJoinApp.java
@@ -18,15 +18,15 @@ public class ReduceJoinApp {
             System.exit(-1);
         }
         Job job = Job.getInstance();
-        job.setJarByClass(JoinJob.class);
+        job.setJarByClass(ReduceJoinApp.class);
         job.setJobName("JoinJob sort");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        job.setPartitionerClass(TextPair.FirstPartitioner.class);
-        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setPartitionerClass(HashPartitioner.class);
+        job.setGroupingComparatorClass(MyComporator.class);
         job.setReducerClass(JoinReducer.class);
-        job.setMapOutputKeyClass(TextPair.class);
+        //job.setMapOutputKeyClass(TextPair.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
