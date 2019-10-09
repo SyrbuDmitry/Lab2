4d08c89a67e36219bbc06c9dcd7d622de03f44ea
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-09 22:51:40 +0300

    Scripted auto-commit on change (2019-10-09 22:51:40) by gitwatch.sh

--- src/main/java/FlightWritable.java
+++ src/main/java/FlightWritable.java
@@ -10,6 +10,6 @@ public class FlightWritable implements Writable {
     private FloatWritable ARR_DELAY, ARR_DELAY_NEW, CANCELLED, AIR_TIME, DISTANCE;
     private Text FL_DATE, UNIQUE_CARRIER, CARRIER, TAIL_NUM, CANCELLATION_CODE;
     void readFiles(DataInput in) throws IOException{
-
+        
     }
 }
