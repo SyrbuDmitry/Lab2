a4f5fb8cd9d3bc45914f0f89cff9a4b4410fd3fc
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-09 22:44:59 +0300

    Scripted auto-commit on change (2019-10-09 22:44:59) by gitwatch.sh

--- src/main/java/FlightWritable.java
+++ src/main/java/FlightWritable.java
@@ -2,5 +2,7 @@ import org.apache.hadoop.io.*;
 
 public class FlightWritable implements Writable {
     private LongWritable YEAR, QUARTER, MONTH, DAY_OF_MONTH,
-            DAY_OF_WEEK, AIRLINE_ID, FL_NUM, ORIGIN_AIRPORT_ID, ORIGIN_AIRPORT_SEQ_ID
+            DAY_OF_WEEK, AIRLINE_ID, FL_NUM, ORIGIN_AIRPORT_ID, ORIGIN_AIRPORT_SEQ_ID,
+            ORIGIN_CITY_MARKET_ID, DEST_AIRPORT_ID, WHEELS_ON, ARR_TIME, ARR_DELAY,
+            ARR_DELAY_NEW,
 }
