a35fcd80d9ef1cd39793645ce963fac1d4073f58
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-09 22:50:46 +0300

    Scripted auto-commit on change (2019-10-09 22:50:46) by gitwatch.sh

--- src/main/java/FlightWritable.java
+++ src/main/java/FlightWritable.java
@@ -1,10 +1,15 @@
 import org.apache.hadoop.io.*;
 
+import java.io.DataInput;
+import java.io.IOException;
+
 public class FlightWritable implements Writable {
     private LongWritable YEAR, QUARTER, MONTH, DAY_OF_MONTH,
             DAY_OF_WEEK, AIRLINE_ID, FL_NUM, ORIGIN_AIRPORT_ID, ORIGIN_AIRPORT_SEQ_ID,
             ORIGIN_CITY_MARKET_ID, DEST_AIRPORT_ID, WHEELS_ON, ARR_TIME;
     private FloatWritable ARR_DELAY, ARR_DELAY_NEW, CANCELLED, AIR_TIME, DISTANCE;
     private Text FL_DATE, UNIQUE_CARRIER, CARRIER, TAIL_NUM, CANCELLATION_CODE;
-    
+    void readFiles(DataInput in) throws IOException{
+
+    }
 }
