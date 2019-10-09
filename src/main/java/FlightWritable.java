9d75f03c36fe7484f35a1281d3c7fa3abccef901
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-09 22:46:31 +0300

    Scripted auto-commit on change (2019-10-09 22:46:31) by gitwatch.sh

--- src/main/java/FlightWritable.java
+++ src/main/java/FlightWritable.java
@@ -4,4 +4,5 @@ public class FlightWritable implements Writable {
     private LongWritable YEAR, QUARTER, MONTH, DAY_OF_MONTH,
             DAY_OF_WEEK, AIRLINE_ID, FL_NUM, ORIGIN_AIRPORT_ID, ORIGIN_AIRPORT_SEQ_ID,
             ORIGIN_CITY_MARKET_ID, DEST_AIRPORT_ID, WHEELS_ON, ARR_TIME;
+    private FloatWritable
 }
