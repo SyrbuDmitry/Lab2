cc5c952dcc020eb0efb28a3820179d30241ce3f3
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 21:54:49 +0300

    Scripted auto-commit on change (2019-10-10 21:54:49) by gitwatch.sh

--- src/main/java/MyComporator.java
+++ src/main/java/MyComporator.java
@@ -5,8 +5,7 @@ import org.apache.hadoop.io.WritableComparator;
 public class MyComporator extends WritableComparator {
     @Override
     public int compare(WritableComparable wc1, WritableComparable wc2) {
-         AirportKey a = (AirportKey)wc1;
-         AirportKey b = (AirportKey)wc2;
-         return a.compareTo(b);
+
+         return 1;
     }
 }
