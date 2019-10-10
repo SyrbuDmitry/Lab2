6f34481dec6f0d3230f4678aea2a32f0752d1e3b
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 18:06:20 +0300

    Scripted auto-commit on change (2019-10-10 18:06:20) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -4,15 +4,19 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+
 import java.io.IOException;
 import java.util.Iterator;
 
-public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
-  @Override
-  protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-    Iterator<Text> iter = values.iterator(); Text systemInfo = new Text(iter.next()); while (iter.hasNext()) {
-      Text call = iter.next();
-      Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
-      context.write(key.getFirst(), outValue);
-    } }
+public class JoinReducer extends Reducer<AirportKey, Text, Text, Text> {
+    @Override
+    protected void reduce(AirportKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text systemInfo = new Text(iter.next());
+        while (iter.hasNext()) {
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
+            //context.write(key.getFirst(), outValue);
+        }
+    }
 }
