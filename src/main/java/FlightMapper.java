4887b59d33c9087d23ca4211a933a20e69b56b18
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 17:11:28 +0300

    Scripted auto-commit on change (2019-10-10 17:11:28) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -13,6 +13,6 @@ public class FlightMapper extends Mapper<AirportKey, Text, FlightWritable, Text>
     protected void map(AirportKey key, Text value, Context context) throws IOException, InterruptedException {
         String columns[] = value.toString().split(",");
         AirportKey flKey = new AirportKey(1,Integer.parseInt(columns[14]));
-        Text delay = new Text(columns[])
+        Text delay = new Text(columns[17]);
         context.write(new TextPair(call.getSystemA().toString(),"1"), new Text(call.toString()));
     } }
