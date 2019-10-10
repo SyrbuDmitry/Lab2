663287531050dbc1ecb87fe175be3a55df4798e1
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 13:06:12 +0300

    Scripted auto-commit on change (2019-10-10 13:06:12) by gitwatch.sh

--- src/main/java/AirportKey.java
+++ src/main/java/AirportKey.java
@@ -1,5 +1,6 @@
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.WritableComparable;
 
 public class AirportKey implements WritableComparable<AirportKey> {
-    private
+    private IntWritable
 }
