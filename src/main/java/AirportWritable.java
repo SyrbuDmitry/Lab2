6399b9287bdb226ea1405244718a32cbc50fa4ed
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 13:20:30 +0300

    Scripted auto-commit on change (2019-10-10 13:20:30) by gitwatch.sh

--- src/main/java/AirportWritable.java
+++ src/main/java/AirportWritable.java
@@ -1,5 +1,20 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirportWritable implements Writable {
-    private 
+    private IntWritable code;
+    private Text description;
+    public void readFields(DataInput in) throws IOException{
+        code.readFields(in);
+        description.readFields(in);
+    }
+    public void write(DataOutput out) throws IOException{
+        code.write(out);
+        description.write(out);
+    }
 }
