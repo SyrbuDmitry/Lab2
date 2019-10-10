c51be9e501a82e9e73247d6338908014592ed7e7
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 20:42:35 +0300

    Scripted auto-commit on change (2019-10-10 20:42:35) by gitwatch.sh

--- src/main/java/ReduceJoinApp.java
+++ src/main/java/ReduceJoinApp.java
@@ -14,7 +14,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class ReduceJoinApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
-            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.err.println("Usage: ReduceJoinApp <input path> <input path> <output path>");
             System.exit(-1);
         }
         Job job = Job.getInstance();
