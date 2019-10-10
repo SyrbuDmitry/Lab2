345e6336277b4972509652728c84d1aae3036eee
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 21:32:19 +0300

    Scripted auto-commit on change (2019-10-10 21:32:19) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -15,7 +15,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportKey, Text> 
         String columns[] = value.toString().split(",");
         if(columns[0].equals("Code"))
             return;
-        AirportKey airKey = new AirportKey( Integer.parseInt(columns[0]),0);
+        AirportKey airKey = new AirportKey( new Text(columns[0]),new IntWritable(0));
         Text description = new Text(columns[1]);
         context.write(airKey,description);
     }
