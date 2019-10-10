e61ae3a8885c8fceaf90788a58b7e11661017e30
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 17:56:01 +0300

    Scripted auto-commit on change (2019-10-10 17:56:01) by gitwatch.sh

--- src/main/java/ReduceJoinApp.java
+++ src/main/java/ReduceJoinApp.java
@@ -23,7 +23,7 @@ public class ReduceJoinApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        job.setPartitionerClass(HashPartitioner.class);
+        job.setPartitionerClass(AirportPartioner.class);
         job.setGroupingComparatorClass(MyComporator.class);
         job.setReducerClass(JoinReducer.class);
         //job.setMapOutputKeyClass(TextPair.class);
