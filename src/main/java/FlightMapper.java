07b054cb68cdfac1de2e3cc784f0b00fa55abe08
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 17:00:08 +0300

    Scripted auto-commit on change (2019-10-10 17:00:08) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -11,6 +11,7 @@ import java.util.StringTokenizer;
 public class FlightMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
         ServiceCall call = new ServiceCall(value);
         context.write(new TextPair(call.getSystemA().toString(),"1"),
         new Text(call.toString()));
