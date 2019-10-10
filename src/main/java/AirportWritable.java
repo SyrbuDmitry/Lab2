8272da4cc1f04bc200c2e2bbfa545eb5799607fe
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 13:24:20 +0300

    Scripted auto-commit on change (2019-10-10 13:24:20) by gitwatch.sh

--- src/main/java/AirportWritable.java
+++ src/main/java/AirportWritable.java
@@ -7,14 +7,14 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class AirportWritable implements Writable {
-    private IntWritable code;
     private Text description;
     public void readFields(DataInput in) throws IOException{
-        code.readFields(in);
         description.readFields(in);
     }
     public void write(DataOutput out) throws IOException{
-        code.write(out);
         description.write(out);
+    }
+    public void setDescription(String description){
+        this.description=new Text(description);
     }
 }
