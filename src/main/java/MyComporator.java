21a617040035b5111387b75bbed27cc6bc09cf16
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-10-10 22:50:10 +0300

    Scripted auto-commit on change (2019-10-10 22:50:10) by gitwatch.sh

--- src/main/java/MyComporator.java
+++ src/main/java/MyComporator.java
@@ -10,6 +10,6 @@ public class MyComporator extends WritableComparator {
     public int compare(WritableComparable wc1, WritableComparable wc2) {
          AirportKey a = (AirportKey)wc1;
          AirportKey b = (AirportKey)wc2;
-         return compare(a.getID(),b.getID());
+         return a.compareTo(b);
     }
 }
